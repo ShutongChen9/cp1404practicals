aaf681c59ab0b7f1eefeb0021e9a67b049fb9976
shutong chen <shutong.chen@my.jcu.edu.au>
2024-11-03 13:17:52 +0800

    Import the class

---
+++ prac_06/languages.py
@@ -0,0 +1,13 @@
+"""
+Word Occurrences
+Estimate: 20 minutes
+Actual:   28 minutes
+"""
+
+from prac_06.programming_language import ProgrammingLanguage
+
+def main():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
