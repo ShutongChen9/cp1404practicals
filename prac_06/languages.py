d8d44bec217a84e68c88fae92863bad362adce82
shutong chen <shutong.chen@my.jcu.edu.au>
2024-11-03 13:31:50 +0800

    Add a new list containing three objects

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -6,8 +6,19 @@ Actual:   28 minutes
 
 from prac_06.programming_language import ProgrammingLanguage
 
+
 def main():
+    """Create instances of ProgrammingLanguage."""
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
     print(python)
+
+    languages = [ruby, python, visual_basic]
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+main()
