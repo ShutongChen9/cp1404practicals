77e41e37863ac7acefb5591c91acbf073947e133
shutong chen <shutong.chen@my.jcu.edu.au>
2024-11-24 21:25:25 +0800

    Add SilverServiceTaxi class tests file

---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,19 @@
+"""
+CP1404/CP5632 Practical
+SilverServiceTaxi class tests
+"""
+
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    """Test SilverServiceTaxi."""
+    taxi = SilverServiceTaxi("Test Fancy Taxi", 100, 2)
+    taxi.drive(18)
+    print(taxi)
+    print(taxi.get_fare())
+
+    expected_fare = 48.78
+    assert taxi.get_fare() == expected_fare, f"Expected fare: {expected_fare}, but got: {taxi.get_fare()}"
+
+main()
